83e52aaf4a2fcf8bdcf03b8a715d4d856bfb2627
Ronald Ris <ronald.tejada@oportunidades.org.sv>
2024-04-12 05:25:32 +0200

    Revert "Webpack Ignore Module notfound - aws amplify TEST"
    This reverts commit f4f393771b7a77df4ab48b870ab3ae0b7124cbdc.

--- main-app/next.config.mjs
+++ main-app/next.config.mjs
@@ -1,24 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "upload.wikimedia.org",
-      "drive.google.com",
-      "publishwhere-tfg.s3.eu-west-3.amazonaws.com",
-      "icons8.com",
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    // Añade esta configuración para ajustar los mensajes de webpack
-    config.stats = {
-      warningsFilter: /module not found/i, // Ignora las advertencias de "module not found"
-    };
-    return config;
-  },
+    images: {
+        domains: ["lh3.googleusercontent.com", "upload.wikimedia.org","drive.google.com", "publishwhere-tfg.s3.eu-west-3.amazonaws.com","icons8.com"],
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback.fs = false;
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
