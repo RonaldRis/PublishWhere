d992c8ce37174285b19f35997c5ead4d195cbb6d
Ronald Ris <ronald.tejada@oportunidades.org.sv>
2024-04-12 05:25:47 +0200

    Revert "Update Webpack Config on next.config"
    This reverts commit 603e197245e7fbd0e8825b25f661a9b62e37c344.

--- main-app/next.config.mjs
+++ main-app/next.config.mjs
@@ -3,12 +3,6 @@ const nextConfig = {
     images: {
         domains: ["lh3.googleusercontent.com", "upload.wikimedia.org","drive.google.com", "publishwhere-tfg.s3.eu-west-3.amazonaws.com","icons8.com"],
     },
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback.fs = false;
-        }
-        return config;
-    },
 };
 
 export default nextConfig;
