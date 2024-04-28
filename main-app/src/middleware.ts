2d9d0d7c4126c83b87c202050bcd06c73a8d78f8
Ronald Tejada <ronald.tejada@oportunidades.org.sv>
2024-04-28 04:32:32 +0200

    Update middleware.ts

--- main-app/src/middleware.ts
+++ main-app/src/middleware.ts
@@ -16,17 +16,22 @@ export const config = {
 };
 
 export async function middleware(request: NextRequest) {
-  const sessionCookie = request.cookies.get("next-auth.session-token");
 
-  if (!sessionCookie) {
-    return Response.redirect(new URL("/", request.url));
-  }
+    //TODO: VALIDAR LAS PAGINAS INTERNAMENTE DENTRO DE CADA UNA SI HAY SESSIÓN O NO Y QUE LO MANDE INICIO
+//   const sessionCookie = request.cookies.get("next-auth.session-token");
 
-  console.log("middleware.ts: nextUrl - ", request.nextUrl);
-  console.log("middleware.ts: url     - ", request.url);
+// //   if (!sessionCookie) {
+// //     return Response.redirect(new URL("/", request.url));
+// //   }
 
-  // If the user is authenticated, allow the request to continue
-  const response = NextResponse.next();
-  response.headers.set("Referrer-Policy", "unsafe-url");
-  return response;
+//   console.log("middleware.ts: nextUrl - ", request.nextUrl);
+//   console.log("middleware.ts: url     - ", request.url);
+
+//   // If the user is authenticated, allow the request to continue
+//   const response = NextResponse.next();
+//   response.headers.set("Referrer-Policy", "unsafe-url");
+//   return response;
+
+return NextResponse.next()
+
 }
