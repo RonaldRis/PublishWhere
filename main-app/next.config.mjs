dfbc1a107a03d2725b3543d45499c7070da6da44
Ronald Tejada <ronald.tejada@oportunidades.org.sv>
2024-05-04 17:55:54 +0200

    Revert "Disabling Errors on Build temporary"
    This reverts commit 6a8f1712483e3aa984908d8d0c6d71d0457ed21a.

--- main-app/next.config.mjs
+++ main-app/next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
     images: {
         domains: ["lh3.googleusercontent.com", "upload.wikimedia.org","drive.google.com", "publishwhere-tfg.s3.eu-west-3.amazonaws.com","icons8.com"],
     },
-    typescript: {
-        ignoreBuildErrors: true, 
-        // TODO: "This is a temporary fix to avoid typescript errors in the build process. Remove this line when the errors are fixed",
-    },
 };
 
 export default nextConfig;
