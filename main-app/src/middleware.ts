a7131b3b29340415a6675c670e0d4b17fca2c02c
Ronald Tejada <ronald.tejada@oportunidades.org.sv>
2024-05-19 23:26:59 +0200

    updating Middleware for hosting and localhost

--- main-app/src/middleware.ts
+++ main-app/src/middleware.ts
@@ -18,20 +18,13 @@ export async function middleware(request: NextRequest) {
 
   request.cookies.getAll().forEach((cookie) => {
     console.log("cookie", cookie)
-  } )
-  
-  console.log("middleware", request.url)
-  //Not the best, but workst
-  if (request.url.includes("publishwhere")) {
-    if (!request.cookies.get("__Secure-next-auth.session-token"))
-      return NextResponse.redirect(new URL('/api/auth/signin', request.url))
+  })
 
+  //Si tiene la primera es en el hosting, si es la segunda es en localhost
+  if (request.cookies.get("__Secure-next-auth.session-token") || request.cookies.get("next-auth.session-token")) {
+    return NextResponse.next()
   }
-  else if (!request.cookies.get("next-auth.session-token")) {
-    return NextResponse.redirect(new URL('/api/auth/signin', request.url))
-  }
-
-  return NextResponse.next()
+  return NextResponse.redirect(new URL('/api/auth/signin', request.url))
 
 }
 
