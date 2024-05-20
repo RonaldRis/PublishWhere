b04998d41c978ad89bc954affadb7f8a09317654
Ronald Tejada <ronald.tejada@oportunidades.org.sv>
2024-05-20 04:17:06 +0200

    Link a la publicacion

--- main-app/src/app/(home)/calendario/_components/RedSocialCardChip.tsx
+++ main-app/src/app/(home)/calendario/_components/RedSocialCardChip.tsx
@@ -10,15 +10,58 @@ import { useContext } from "react";
 import { toast } from "sonner";
 import { useLocalStorage } from "usehooks-ts";
 
+function ContentRedSociaChip({ social }: { social: ISocialMediaAccount }) {
+    return (
+        <>
+            <Image
+                src={social.thumbnail}
+                width={50}
+                height={50}
+                className="rounded-full"
+                alt="Description of the image"
+                style={{ objectFit: "contain" }}
+            />
+
+            <div>
+
+                <p className="text-center font-bold m-0 text-xl pb-2">
+                    {social.name}
+                </p>
+                <div className="relative w-full">
+                    <p className="text-center mx-6">{social.provider}</p>
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+
 export default function RedSocialCardChip({ social }: { social: ISocialMediaAccount }) {
 
     const color = labelsProviderToColor[social.provider];
     const { selectedRedesSocialesList,
+        selectedEvent,
         setSelectedRedesSocialesList } = useContext(CalendarioContext);
+
+    var url = "/";
+    if (selectedEvent) {
+
+        selectedEvent.socialMedia.map((socialMedia) => {
+            if (socialMedia.socialMedia._id === social._id) {
+                url = socialMedia.urlPost;
+            }
+        });
+    }
 
     const isSelected = selectedRedesSocialesList.some((red) => red._id === social._id);
 
     const handlerClick = async () => {
+
+        if(selectedEvent)
+            return; //No editable
+
+
         if (selectedRedesSocialesList.some((red) => red._id === social._id)) {
             const filterList = selectedRedesSocialesList.filter((red) => red._id !== social._id);
             setSelectedRedesSocialesList(filterList);
@@ -30,25 +73,16 @@ export default function RedSocialCardChip({ social }: { social: ISocialMediaAcco
     return (
         <Card className={`h-full border-4 border-${color}-500 ${isSelected ? "bg-" + color + "-500" : ""}`} key={social._id} onClick={handlerClick}>
             <CardContent className="flex justify-between items-center p-3 h-full">
-                <Image
-                    src={social.thumbnail}
-                    width={50}
-                    height={50}
-                    className="rounded-full"
-                    alt="Description of the image"
-                    style={{ objectFit: "contain" }}
-                />
 
-                <div>
+                {selectedEvent ?
 
-                    <p className="text-center font-bold m-0 text-xl pb-2">
-                        {social.name}
-                    </p>
-                    <div className="relative w-full">
-                        <p className="text-center mx-6">{social.provider}</p>
+                    <Link href={url}>
+                        <ContentRedSociaChip social={social} />
+                    </Link>
+                    :
+                    <ContentRedSociaChip social={social} />
+                }
 
-                    </div>
-                </div>
             </CardContent>
         </Card>
     );
